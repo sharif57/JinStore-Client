12e6f44482b5f55f1a280f2231b0bf66c4fea331
sharif57 <sharifmahamud577951@gmail.com>
2024-10-23 17:30:44 +0600

    top selling data get

--- src/Trending/Trending.jsx
+++ src/Trending/Trending.jsx
@@ -13,33 +13,37 @@ const Trending = () => {
     return <div>
         <div className="grid grid-cols-1 lg:grid-cols-4 gap-4 p-4">
             {
-                tops.map((top, index) => <article key={index} className="relative overflow-hidden rounded-lg shadow transition hover:shadow-lg">
-                    <img
-                        alt=""
-                        src="https://images.unsplash.com/photo-1661956602116-aa6865609028?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80"
-                        className="absolute inset-0 h-full w-full object-cover"
-                    />
+                tops.map((top, index) => (
+                    <article
+                        key={index}
+                        className="relative overflow-hidden h-[500px] w-full rounded-lg shadow transition hover:shadow-lg"
+                    >
+                        <img
+                            alt=""
+                            src={top.image}
+                            className="absolute inset-0 h-full w-full object-cover"
+                        />
 
-                    <div className="relative bg-gradient-to-t from-gray-900/50 to-gray-900/25 pt-32 sm:pt-48 lg:pt-64">
-                        <div className="p-4 sm:p-6">
-                            <time datetime="2022-10-10" className="block text-xs text-white/90"> 10th Oct 2022 </time>
+                        <div className="relative bg-gradient-to-t from-gray-900/50 to-gray-900/25 pt-32 sm:pt-48 lg:pt-64 h-[500px]">
+                            <div className="p-4 sm:p-6">
+                                <time dateTime="2022-10-10" className="block text-xs text-white/90">
+                                    {top.currentTime}
+                                </time>
 
-                            <a href="#">
-                                <h3 className="mt-0.5 text-lg text-white">How to position your furniture for positivity</h3>
-                            </a>
+                                <a href="#">
+                                    <h3 className="mt-0.5 text-lg text-white">{top.name}</h3>
+                                </a>
 
-                            <p className="mt-2 line-clamp-3 text-sm/relaxed text-white/95">
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
-                                pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis
-                                quidem, mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius
-                                atque dignissimos. Molestias explicabo corporis voluptatem?
-                            </p>
+                                <p className="mt-2 line-clamp-3 text-sm/relaxed text-white/95">
+                                    {top.description}
+                                </p>
+                            </div>
                         </div>
-                    </div>
-                </article>)
+                    </article>
+                ))
             }
-
         </div>
+
     </div>;
 };
 export default Trending;
